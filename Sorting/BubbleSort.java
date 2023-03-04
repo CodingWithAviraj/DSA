5f31cca93c0fed92aff2409471d91e151d5f091e
Aviraj <panditaviraj99@gmail.com>
2023-03-04 18:51:55 +0530

    updating bubbleSort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,7 +2,7 @@
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] arr = {8,7,6,5, 0,4,3,2,1};
+		int[] arr = {8,7,6,5,4,3,2,1};
 		int n = arr.length;
 		
 		bubbleSort(arr, n);
@@ -15,13 +15,16 @@ public class BubbleSort {
 	private static void bubbleSort(int[] arr, int n) {
 		
 		for(int i=0; i<n-1; i++) {
+			boolean flag = false;
 			for(int j=0; j<n-i-1; j++) {
 				if(arr[j] > arr[j+1]) {
+					flag = true;
 					int temp = arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = temp;
 				}
 			}
+			if(flag == false)break;
 		}
 	}
 
